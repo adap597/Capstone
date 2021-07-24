000883e64bdfaf0f7825e1e64768f5552b22a1b0
adap597 <adap194567@gmail.com>
2021-07-24 09:10:27 -0700

    updated test app database path

--- test_app.py
+++ test_app.py
@@ -19,7 +19,7 @@ class CastingTest(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "casting_test"
-        self.database_path = "postgres:///{}".format(self.database_name)
+        self.database_path = "postgresql:///{}".format(self.database_name)
         setup_db(self.app, self.database_path)
 
         with self.app.app_context():
