6671bdaa07fbbb164f75e544e4f2f7afd3067899
adap597 <adap194567@gmail.com>
2021-08-03 14:51:17 -0700

    Updated auth.py to remove Auth0 info and created config file

--- auth.py
+++ auth.py
@@ -4,11 +4,12 @@ from flask import request, _request_ctx_stack
 from functools import wraps
 from jose import jwt
 from urllib.request import urlopen
+from config import AUTH0_DOMAIN, ALGORITHMS, API_AUDIENCE, CLIENT_ID
 
-AUTH0_DOMAIN = 'udacfsnd.us.auth0.com'
-ALGORITHMS = ['RS256']
-API_AUDIENCE = 'Casting'
-CLIENT_ID = 'nBEglxlDSNtkW33pVuNyFYpXhPdz6PzG'
+AUTH0_DOMAIN = 'AUTH0_DOMAIN'
+ALGORITHMS = 'ALGORITHMS'
+API_AUDIENCE = 'API_AUDIENCE'
+CLIENT_ID = 'CLIENT_ID'
 
 def get_json_data(url):
     operUrl = urlopen(url)
--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@ export SECRET='ihxq2MKyjX_XRxdxQIbXLYD1rJohSUpY9QGwbW-AZC1kV0VP-YiZVk4nxvyOEIfv'
 export AUTH0_DOMAIN="udacfsnd.us.auth0.com" 
 export ALGORITHMS="RS256"
 export API_AUDIENCE="Casting" 
+export CLIENT_ID='nBEglxlDSNtkW33pVuNyFYpXhPdz6PzG'
 
 export DATABASE_URL="postgres://gtppoatzffgybr:849f56f797fa3798c249b5b012c5c70faf205b9323a9c337c6bf9ecbf3b3bfc3@ec2-54-236-137-173.compute-1.amazonaws.com:5432/d5jabkfovb1acg"
 export FLASK_APP=app
